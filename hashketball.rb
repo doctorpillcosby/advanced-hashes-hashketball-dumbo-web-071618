d6f8c686c8865eea22d197ea4419f0b20d4e9a7d
Tyrell Gordon <gordontyrell1993@gmail.com>
2018-07-09 06:20:09 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,141 +1,133 @@
 game = {
-  :team1 => {
-    :name => "The Ragin' Cajuns",
-    :colors => ["red", "yellow"],
+  :knicks => {
+    :colors => ["orange", "blue"],
+
     :players => {
-      :player1 => {
-        :name => "Mike DeLaRosa",
-        :number => 45,
-        :shoe_size => 12,
+      "Carmelo Anthony" => {
+        :number => 7,
+        :shoe => 13,
         :stats => {
-          :points => 12,
-          :rebounds => 4,
-          :assists => 2,
-          :steals => 0,
-          :blocks => 0,
-          :slam_dunks => 6
-        }
-      },
-      :player2 => {
-        :name => "Bill Nye",
-        :number => 12,
-        :shoe_size => 11,
-        :stats => {
-          :points => 28,
+          :points => 10,
           :rebounds => 9,
-          :assists => 0,
-          :steals => 0,
-          :blocks => 0,
-          :slam_dunks => 10
+          :assists => 8,
+          :steals => 7,
+          :blocks => 6,
+          :slam_dunks => 5
         }
       },
-      :player3 => {
-        :name => "Conor Walsh",
-        :number => 15,
-        :shoe_size => 10,
+      "Amare Stoudemire" => {
+        :number => 1,
+        :shoe => 16,
         :stats => {
-          :points => 4,
-          :rebounds => 4,
-          :assists => 6,
-          :steals => 4,
-          :blocks => 9,
-          :slam_dunks => 1
+          :points => 10000,
+          :rebounds => 9,
+          :assists => 8,
+          :steals => 7,
+          :blocks => 6,
+          :slam_dunks => 5
         }
       },
-      :player4 => {
-        :name => "James Blake",
-        :number => 67,
-        :shoe_size => 12,
+      "Iman Shumpert" => {
+        :number => 21,
+        :shoe => 14,
         :stats => {
-          :points => 0,
-          :rebounds => 1,
-          :assists => 0,
-          :steals => 1,
-          :blocks => 4,
-          :slam_dunks => 0
+          :points => 10,
+          :rebounds => 9,
+          :assists => 8,
+          :steals => 7,
+          :blocks => 6,
+          :slam_dunks => 5
         }
       },
-      :player5 => {
-        :name => "Ernest Feinhorn",
-        :number => 52,
-        :shoe_size => 11,
+      "Tyson Chandler" => {
+        :number => 6,
+        :shoe => 12,
         :stats => {
-          :points => 8,
-          :rebounds => 1,
-          :assists => 1,
-          :steals => 0,
-          :blocks => 2,
-          :slam_dunks => 1
+          :points => 10,
+          :rebounds => 9,
+          :assists => 8,
+          :steals => 7,
+          :blocks => 6,
+          :slam_dunks => 5
+        }
+      },
+      "Pablo Prigioni asfadsfdsafdasfadsas" => {
+        :number => 9,
+        :shoe => 11,
+        :stats => {
+          :points => 10,
+          :rebounds => 9,
+          :assists => 8,
+          :steals => 37,
+          :blocks => 6,
+          :slam_dunks => 5
         }
       }
     }
   },
-  :team2 => {
-    :name => "The Mile High Skyscrapers",
-    :colors => ["blue", "white"],
+
+
+  :celtics => {
+    :colors => ["green", "white"],
+    
     :players => {
-      :player1 => {
-        :name => "Al Gore",
+      "Brandon Bass" => {
+        :number => 30,
+        :shoe => 13,
+        :stats => {
+          :points => 10,
+          :rebounds => 9,
+          :assists => 8,
+          :steals => 20,
+          :blocks => 6,
+          :slam_dunks => 5
+        }
+      },
+      "Keith Bogans" => {
         :number => 4,
-        :shoe_size => 15,
+        :shoe => 12,
         :stats => {
-          :points => 3,
-          :rebounds => 4,
-          :assists => 5,
-          :steals => 1,
-          :blocks => 0,
-          :slam_dunks => 0
-        }
-      },
-      :player2 => {
-        :name => "King George",
-        :number => 55,
-        :shoe_size => 11,
-        :stats => {
-          :points => 18,
-          :rebounds => 4,
-          :assists => 4,
-          :steals => 1,
-          :blocks => 3,
-          :slam_dunks => 4
-        }
-      },
-      :player3 => {
-        :name => "Michael Giordan",
-        :number => 23,
-        :shoe_size => 13,
-        :stats => {
-          :points => 0,
-          :rebounds => 0,
-          :assists => 0,
-          :steals => 0,
-          :blocks => 0,
-          :slam_dunks => 0
-        }
-      },
-      :player4 => {
-        :name => "Guy Smith",
-        :number => 68,
-        :shoe_size => 10,
-        :stats => {
-          :points => 23,
-          :rebounds => 7,
-          :assists => 9,
-          :steals => 0,
-          :blocks => 0,
-          :slam_dunks => 9
-        }
-      },
-      :player5 => {
-        :name => "Billie Vacation",
-        :number => 32,
-        :shoe_size => 12,
-        :stats => {
-          :points => 18,
-          :rebounds => 12,
-          :assists => 3,
+          :points => 10,
+          :rebounds => 9,
+          :assists => 8,
           :steals => 7,
-          :blocks => 0,
+          :blocks => 6,
+          :slam_dunks => 5
+        }
+      },
+      "Avery Bradley" => {
+        :number => 0,
+        :shoe => 14,
+        :stats => {
+          :points => 10,
+          :rebounds => 9,
+          :assists => 8,
+          :steals => 7,
+          :blocks => 6,
+          :slam_dunks => 5
+        }
+      },
+      "Jeff Green" => {
+        :number => 8,
+        :shoe => 20,
+        :stats => {
+          :points => 10,
+          :rebounds => 4,
+          :assists => 8,
+          :steals => 7,
+          :blocks => 6,
+          :slam_dunks => 5
+        }
+      },
+      "Marshon Brooks" => {
+        :number => 12,
+        :shoe => 15,
+        :stats => {
+          :points => 10,
+          :rebounds => 9,
+          :assists => 8,
+          :steals => 7,
+          :blocks => 6,
           :slam_dunks => 5
         }
       }
@@ -143,172 +135,213 @@ game = {
   }
 }
 
-# Using the power of Ruby, and the Hashes you created above, answer the following questions:
+# game
+  #team
+    #colors --> ARR
+    #players
+      #"Person"
+        #number
+        #shoe
+        #stats
+          #points
+          #rebounds
+          #assists
+          #steals
+          #blocks
+          #slam_dunks
+
+
 # Return the number of points scored for any player:
-#
-def points_of_player(player_name, game)
-  game.each do |team, team_hash|
-    team_hash[:players].each do |player, player_hash|
-      if player_hash[:name] == player_name
-        return player_hash[:stats][:points]
+def points_for(player, game)
+  game.each_value do |team, team_info|
+    team[:players].each do |name, player_hash|
+      if name == player
+        points = player_hash[:stats][:points]
+        puts "#{player} scored #{points} points."
       end
     end
   end
 end
-
-p points_of_player("Michael Giordan", game)
 
 # Return the shoe size for any player:
-#
-def shoe_size_of_player(player_name, game)
-  game.each do |team, team_hash|
-    team_hash[:players].each do |player, player_hash|
-      if player_hash[:name] == player_name
-        return player_hash[:shoe_size]
+def shoe_size_for(player, game)
+  game.each_value do |team|
+    team[:players].each do |name, player_hash|
+      if name == player
+        size = player_hash[:shoe].to_s
+        puts "#{player} wears size #{size} shoe"
       end
     end
   end
 end
-
-p shoe_size_of_player("King George", game)
 
 # Return both colors for any team:
-#
-def team_colors(team_name, game)
-  game.each do |team, team_hash|
-    if team_hash[:name] == team_name
-      return (team_hash[:colors].collect do |color|
-        color
-      end)
+def team_colors_for(team, game)
+  game.each do |team_name, team_info|
+    if team_name.to_s == team
+      tcolors = team_info[:colors].join(" ")
+      puts "The #{team_name}' team colors are #{tcolors}."
     end
   end
 end
-
-p team_colors("The Ragin' Cajuns", game)
-
+ 
 # Return both teams names:
-#
-def team_names(game)
-  game.collect do |team, team_hash|
-    team_hash[:name]
+def what_teams?(game)
+  team_arr = []
+  game.each_key do |team|
+    team_arr << team.to_s
   end
+
+  teams_string = team_arr.join(" and ")
+  puts "The teams playing are #{teams_string}."
 end
-
-p team_names(game)
-
+ 
 # Return all the player numbers for a team:
-#
-def all_team_players(team_name, game)
-  game.each do |team, team_hash|
-    if team_hash[:name] == team_name
-      return (team_hash[:players].collect do |player, player_hash|
-        player_hash[:name]
-      end)
-    end
+def players_on_team(team, game)
+  team_name = team.to_sym
+  
+  team_members = []
+  game[team_name][:players].each_key do |player|
+    team_members << player
   end
+
+  team_string = team_members.join(", ")
+
+  puts "The players on the #{team_name} are #{team_string}."
 end
-
-p all_team_players("The Ragin' Cajuns", game)
-
+ 
 # Return all the stats for a player:
-#
-def all_stats_for_player(player_name, game)
-  game.each do |team, team_hash|
-    team_hash[:players].each do |player, player_hash|
-      if player_hash[:name] == player_name
-        return player_hash[:stats]
+def player_stats_for(player, game)
+    game.each_value do |team|
+    team[:players].each do |name, player_hash|
+      if name == player
+        stats = player_hash[:stats].to_s
+        puts "#{player}'s stats are #{stats}"
       end
     end
   end
 end
-
-p all_stats_for_player("Bill Nye", game)
-
+ 
 # Return the rebounds for the player with the largest shoe size
-#
-def rebounds_for_largest_shoe_size(game)
-  max_player = nil
-  game.each do |team, team_hash|
-    team_hash[:players].each do |player, player_hash|
-      max_player ||= player_hash
-      max_player = player_hash if player_hash[:shoe_size] > max_player[:shoe_size]
-    end
+def rebounds_for_bigfoot(game)
+  biggest_foot = ["Default Name", 0, 0] # [Player, Shoe, Rebounds]
+
+  game.each_value do |team|
+    team[:players].each do |name, player_hash|
+
+      if player_hash[:shoe] > biggest_foot[1]
+        biggest_foot[0] = name
+        biggest_foot[1] = player_hash[:shoe]
+        biggest_foot[2] = player_hash[:stats][:rebounds]
+      end
+
+    end 
   end
 
-  max_player[:stats][:rebounds]
-end
+  bigfoot_name = biggest_foot[0]
+  bigfoot_shoe = biggest_foot[1]
+  bigfoot_rebounds = biggest_foot[2]
 
-p rebounds_for_largest_shoe_size(game)
+  puts "#{bigfoot_name} has the biggest foot (size #{bigfoot_shoe}!) and has made #{bigfoot_rebounds} rebounds. Yowzahz! Mad rigorous."
+
+end
+ 
+rebounds_for_bigfoot(game)
 
 # Bonus Questions: define methods to return the answer to the following questions:
 # Which player has the most points?
-#
-def player_with_most_points(game)
-  max_player = nil
-  game.each do |team, team_hash|
-    team_hash[:players].each do |player, player_hash|
-      max_player ||= player_hash
-      max_player = player_hash if player_hash[:stats][:points] > max_player[:stats][:points]
+def most_points?(game)
+  highest_scorer = ["Default Name", 0] # [Name, high score]
+
+  game.each_value do |team|
+    team[:players].each do |name, player_hash|
+
+      if player_hash[:stats][:points] > highest_scorer[1]
+        highest_scorer[0] = name
+        highest_scorer[1] = player_hash[:stats][:points]
+      end
+
     end
   end
 
-  max_player[:name]
+  top_name = highest_scorer[0]
+  top_score = highest_scorer[1]
+
+  puts "The highest scorer is #{top_name} and his score is #{top_score}. Daayuum!! Mad rigorous."
+
 end
-
-p player_with_most_points(game)
-
+ 
 # Which team has the most points?
-#
-def team_with_most_points(game)
-  max_team = nil
-  game.each do |team, team_hash|
-    sum = 0
-    team_hash[:players].each do |player, player_hash|
-      sum += player_hash[:stats][:points]
-    end
+def current_winning_team(game)
+  score_keeper = {}
+  
+  game.each do |team, team_info|
+    score = 0
 
-    team_hash[:sum] = sum
-    max_team ||= team_hash
-    max_team = team_hash if team_hash[:sum] > max_team[:sum]
+    team_info[:players].each_value do |player_info, info|
+      score += player_info[:stats][:points]
+    end
+    
+    score_keeper[team] = score
+
   end
 
-  max_team[:name]
-end
+  high_score = score_keeper.values.max
+  high_scorer = score_keeper.key(high_score)
 
-p team_with_most_points(game)
+  puts "The winning team is #{high_scorer} with a score of #{high_score}. Yasss."
+
+end
+ 
+current_winning_team(game)
 
 # Which player has the longest name?
-#
-def player_with_longest_name(game)
-  max_player = nil
-  game.each do |team, team_hash|
-    team_hash[:players].each do |player, player_hash|
-      max_player ||= player_hash
-      max_player = player_hash if player_hash[:name].length > max_player[:name].length
+def longest_name(game)
+  longname = ["Default", 0] #[Name, length]
+
+  game.each_value do |team, team_info|
+    team[:players].each_key do |name|
+      if name.length > longname[1]
+        longname[0] = name
+        longname[1] = name.length
+      end
     end
   end
 
-  max_player[:name]
+  bigname = longname[0]
+  bigname_length = longname[1]
+  puts "The longest name is #{bigname} with a length of #{bigname_length} characters! DAS IT."
+
+  bigname
 end
 
-p player_with_longest_name(game)
+longest_name(game)
 
 # Super Bonus:
-# Write a method that returns true if the player with the longest name had the most steals:
-#
-def player_with_most_steals_and_name(game)
-  max_player = nil
-  game.each do |team, team_hash|
-    team_hash[:players].each do |player, player_hash|
-      max_player ||= player_hash
-      max_player = player_hash if player_hash[:stats][:steals] > max_player[:stats][:steals]
+# Write a method that returns true if the player with the longest name had the most steals.
+def most_steals(game)
+  big_steals = ["Default", 0] #[Name, steals]
+
+  game.each_value do |team, team_info|
+    team[:players].each do |name, player_info|
+      if player_info[:stats][:steals] > big_steals[1]
+        big_steals[0] = name
+        big_steals[1] = player_info[:stats][:steals]
+      end
     end
   end
 
-  max_player[:name] == player_with_longest_name(game)
+  high_stealer = big_steals[0]
+  high_steal_num = big_steals[1]
+  puts "#{high_stealer} stole #{high_steal_num} times, the most out of them all!"
+
+  high_stealer
 end
 
+most_steals(game)
 
+def longest_name_most_steals(game)
+  most_steals(game) == longest_name(game) ? true : false
+end
 
-
-
+puts longest_name_most_steals(game)
